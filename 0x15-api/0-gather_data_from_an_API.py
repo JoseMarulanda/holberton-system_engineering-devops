ef3ec4943f45205a05b77eef8bd387e86dc20546
JoseMarulanda <josejumarumol@gmail.com>
2020-02-03 23:43:19 -0500

    fix output

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-Python script that, using an API, for a given employee ID
-returns information about his/her TODO list progress
-"""
+""" Script that uses JSONPlaceholder API to get information about employee """
 import requests
 import sys
 
@@ -10,19 +7,18 @@ import sys
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     user = '{}users/{}'.format(url, sys.argv[1])
-    r = requests.get(user)
-    json_obj = r.json()
-    print("Employee {} is done with tasks".format(json_obj.get('name')),
-          end="")
+    res = requests.get(user)
+    json_o = res.json()
+    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
 
     todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    r = requests.get(todos)
-    tasks = r.json()
-    tasks_list = []
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
     for task in tasks:
         if task.get('completed') is True:
-            tasks_list.append(task)
+            l_task.append(task)
 
-    print("({}/{}):".format(len(tasks_list), len(tasks)))
-    for task in tasks_list:
+    print("({}/{}):".format(len(l_task), len(tasks)))
+    for task in l_task:
         print("\t {}".format(task.get("title")))
