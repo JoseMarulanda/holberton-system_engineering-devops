16661713b71c6cb3f179892c9dbb3bafd75a5335
JoseMarulanda <josejumarumol@gmail.com>
2020-02-03 23:54:03 -0500

    fix requirement

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,29 +1,27 @@
 #!/usr/bin/python3
-"""
-Python script that, using an API, for a given employee ID
-returns information about his/her TODO list progress
-"""
+""" Script that uses JSONPlaceholder API to get information about employee """
+import csv
 import requests
 import sys
-import csv
 
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     userid = sys.argv[1]
     user = '{}users/{}'.format(url, userid)
-    r = requests.get(user)
-    json_obj = r.json()
-    name = json_obj.get('username')
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
 
     todos = '{}todos?userId={}'.format(url, userid)
-    r = requests.get(todos)
-    tasks = r.json()
-    tasks_list = []
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
     for task in tasks:
-        tasks_list.append([userid, name,
-                           task.get('completed'),
-                           task.get('title')])
+        l_task.append([userid,
+                       name,
+                       task.get('completed'),
+                       task.get('title')])
 
     filename = '{}.csv'.format(userid)
     with open(filename, mode='w') as employee_file:
@@ -31,5 +29,5 @@ if __name__ == "__main__":
                                      delimiter=',',
                                      quotechar='"',
                                      quoting=csv.QUOTE_ALL)
-        for task in tasks_list:
+        for task in l_task:
             employee_writer.writerow(task)
