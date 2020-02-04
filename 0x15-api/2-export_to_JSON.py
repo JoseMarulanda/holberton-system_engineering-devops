13406368bcdbc00d3d03a7e68d491d768da399a6
JoseMarulanda <josejumarumol@gmail.com>
2020-02-03 23:55:52 -0500

    fix requirement

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,32 +1,29 @@
 #!/usr/bin/python3
-"""
-Python script that, using an API, for a given employee ID
-returns information about his/her TODO list progress
-"""
+""" Script that uses JSONPlaceholder API to get information about employee """
 import json
-import sys
 import requests
+import sys
 
 
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
     userid = sys.argv[1]
     user = '{}users/{}'.format(url, userid)
-    r = requests.get(user)
-    json_obj = r.json()
-    name = json_obj.get('username')
+    res = requests.get(user)
+    json_o = res.json()
+    name = json_o.get('username')
 
     todos = '{}todos?userId={}'.format(url, userid)
-    r = requests.get(todos)
-    tasks = r.json()
-    tasks_list = []
+    res = requests.get(todos)
+    tasks = res.json()
+    l_task = []
     for task in tasks:
         dict_task = {"task": task.get('title'),
                      "completed": task.get('completed'),
                      "username": name}
-        tasks_list.append(dict_task)
+        l_task.append(dict_task)
 
-    d_task = {str(userid): tasks_list}
+    d_task = {str(userid): l_task}
     filename = '{}.json'.format(userid)
     with open(filename, mode='w') as f:
         json.dump(d_task, f)
